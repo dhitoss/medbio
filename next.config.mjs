8de66e460e2ff36e62c33be612acba748cc7eefb
Simon <77901095+jfsimon-pro@users.noreply.github.com>
2025-02-19 18:13:31 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
+import { join } from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configuração para servir arquivos estáticos
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: join(process.cwd()),
+  },
   // Log para verificar a configuração
   async headers() {
     console.log('Next.js config: Verificando pasta public');
